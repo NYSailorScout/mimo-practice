904df39f4fec95983214e1506544262eb599a9d9
NYCLouisW <68522766+NYSailorScout@users.noreply.github.com>
2024-09-13 17:11:16 -0400

    Update script.js
    Add miles converter js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let inputValue = 10;
+let milesToKm = false;
 
+let result = 0;
+
+if (milesToKm) {
+  result = inputValue * 1.60934;
+} else {
+  result = inputValue / 1.60934;
+}
+
+const resultString = inputValue + " miles are " + result + " km";
+console.log(resultString);
