56c02d701188b64c21cbcbdfdabcde6dbea91e4f
NYCLouisW <68522766+NYSailorScout@users.noreply.github.com>
2024-09-21 17:46:19 -0400

    Update script.js
    add guessing game logic

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+/*
 const inputValue = 10;
 const milesToKm = true;
 let result = 0;
@@ -13,7 +16,23 @@ console.log(resultString);
 
 const resultElement = document.getElementById('resultElement');
 resultElement.innerHTML = resultString;
+*/
 
 function checkGuess() {
-  console.log("Good guess!");
+  let inputElement = document.getElementById("guess");
+  let feedbackElement = document.getElementById("feedback");
+  let guess = inputElement.value;
+
+  if (guess === randomNumber) {
+     feedbackElement.style.color("green");
+     feedbackElement.innerHTML = "You guessed it!";
+  } else if (guess < randomNumber) {
+     feedbackElement.style.color("red");
+     feedbackElement.innerHTML = "Too low!";
+  } else {
+     feedbackElement.style.color("red");
+     feedbackElement.innerHTML = "Too high!";
+  }
+  
 }
+
